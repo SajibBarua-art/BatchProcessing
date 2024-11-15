b3201e8d2dce9ac6e4d96aa8b019c423ed781f04
SajibBarua-art <sajibbaruanstu@gmail.com>
2024-11-15 13:20:49 +0600

    first commit

---
+++ src/main/java/com/example/batchprocessing/config/CustomerProcessor.java
@@ -0,0 +1,18 @@
+package com.example.batchprocessing.config;
+
+import com.example.batchprocessing.entity.Customer;
+import org.springframework.batch.item.ItemProcessor;
+
+// <Customer, Customer> means read and write item as a customer
+public class CustomerProcessor implements ItemProcessor<Customer, Customer> {
+
+    // To process only the records which country are United States
+    @Override
+    public Customer process(Customer customer) throws Exception {
+        if(customer.getCountry().equals("United States")) {
+            return customer;
+        }else{
+            return null;
+        }
+    }
+}
